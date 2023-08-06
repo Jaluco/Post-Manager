da53af7261b8f116b9434486a4bd53c3592d9b47
jluco <jluco@altavoz.net>
2023-08-06 19:40:14 -0400

    Fix: rubocop recommended changes

--- post_manager_api/app/controllers/posts_controller.rb
+++ post_manager_api/app/controllers/posts_controller.rb
@@ -1,39 +1,40 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :destroy]
-  
-    # GET /posts
-    def index
-      @posts = Post.all
-      render json: @posts
-    end
-  
-    # POST /posts
-    def create
-      @post = Post.new(post_params)
-  
-      if @post.save
-        render json: @post, status: :created
-      else
-        render json: @post.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # DELETE /posts/:id
-    def destroy
-      @post.destroy
-      render json: @post
-    end
-  
-    private
-  
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-  
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:name, :description)
+  before_action :set_post, only: %i[show destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
   end
-  
+
+  # DELETE /posts/:id
+  def destroy
+    @post.destroy
+    render json: @post
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:name, :description)
+  end
+end
--- post_manager_api/config/routes.rb
+++ post_manager_api/config/routes.rb
@@ -1,4 +1,5 @@
-Rails.application.routes.draw do
-  resources :posts, only: [:index, :create, :destroy]
-end
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
+  resources :posts, only: %i[index create destroy]
+end
